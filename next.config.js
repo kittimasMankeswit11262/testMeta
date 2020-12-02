37ef31566e05ddb5cbae1b376c07c78aa76acf11
Kittimas Mankeswit <best@jenosize.com>
2020-12-02 18:12:00 +0700

    :

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
-// module.exports = {
-//     i18n: {
-//         locales: ['th', 'en'],
-//         defaultLocale: 'th'
-//     }
-// }
-// const { nextI18NextRewrites } = require('next-i18next/rewrites')
-
-// const localeSubpaths = {}
-
-// module.exports = {
-//     rewrites: async () => nextI18NextRewrites(localeSubpaths),
-//     publicRuntimeConfig: {
-//         localeSubpaths,
-//     },
-// }
-
-const { nextI18NextRewrites } = require('next-i18next/rewrites')
-
-const localeSubpaths = {}
-
 module.exports = {
-  rewrites: async () => nextI18NextRewrites(localeSubpaths),
-  publicRuntimeConfig: {
-    localeSubpaths,
-  },
+    i18n: {
+        locales: ['th', 'en'],
+        defaultLocale: 'th'
+    }
 }
